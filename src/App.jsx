3b3033c459495e7a1eea78dc5fa1fb5da6b41ace
PH <paulofera159@gmail.com>
2024-11-09 19:39:42 -0300

    Att

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
-import Header from './components/Header/Header.jsx';
 import Footer from './components/footer/Footer';
 import Rotas from './routes/Rotas';
+import Header from './components/header/Header';
 
 const App = () => {
   return (
