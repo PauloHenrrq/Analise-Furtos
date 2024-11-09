567f6c458e3f834d0416d3076b09b0bc87746956
PH <paulofera159@gmail.com>
2024-11-09 19:22:51 -0300

    Att

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
-import Header from './components/Header/Header.jsx';
 import Footer from './components/footer/Footer';
 import Rotas from './routes/Rotas';
 
@@ -9,7 +8,6 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className="app-container">
-        <Header />
         <main className="content">
           <Rotas />
         </main>
